32e20fc6d7e99f71a96f1e1870efdef7defe6dd7
Ayoub Taouaf <ataouaf@e1r3p2.1337.ma>
2024-10-08 12:08:38 +0100

    fix:merge

---
+++ frontend/app/auth/callback.tsx
@@ -0,0 +1,9 @@
+// pages/auth/callback.tsx (or app/auth/callback.tsx)
+
+import LoginCallback from "../Components/loginCallback"; // Adjust path as necessary
+
+const CallbackPage = () => {
+  return <LoginCallback />;
+};
+
+export default CallbackPage;
---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './login';
+import '../globals.css';
+
+function App() {
+    return (
+        <div className="bg-[#393E46] h-screen flex justify-center items-center">
+            <Login />
+        </div>
+    )
+}
+
+export default App;
