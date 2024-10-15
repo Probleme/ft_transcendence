a8eeeff4cbc9cb76839e769a46fd61936eefb23a
Yousra Ibarki <yousra.ibarki@gmail.com>
2024-10-15 10:24:58 +0100

    game speed added

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eed60608f50b2d6f"; }
+/******/ 		__webpack_require__.h = function() { return "3c52ea2dd0751294"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
